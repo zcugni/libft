bdf1857cd01385cb4b4f468aadf5b5021b4b4ba9
Zoé Cugni <zcugni@student.42.fr>
2019-01-14 14:47:58 +0100

    Update ft_strsplit
    L'ancienne version était moins concise.

--- srcs/str/ft_strsplit.c
+++ srcs/str/ft_strsplit.c
@@ -19,64 +19,59 @@
 
 static int	count_words(char const *s, char c)
 {
-	int first_word;
 	int nb_words;
 	int i;
 
 	i = 0;
-	first_word = 1;
 	nb_words = 0;
 	while (s[i])
 	{
-		if (s[i] != c)
-			if (first_word == 1 || s[i - 1] == c)
-			{
-				first_word = 0;
-				nb_words++;
-			}
-		i++;
+		while (s[i] && s[i] != c)
+			i++;
+		nb_words++;
+		while (s[i] == c)
+			i++;
 	}
 	return (nb_words);
 }
 
-static void	add_words(char const *s, char c, char **tmp_arr)
+static void	fill_arr(char const *s, char c, char **arr)
 {
-	int		first_word;
-	size_t	size;
-	int		nb_words;
+	size_t	j;
+	size_t	word;
 	size_t	i;
-
-	nb_words = 0;
-	first_word = 1;
+	
 	i = 0;
-	while (s[i])
+	j = 0;
+	word = 0;
+	while (str[j])
 	{
-		if (s[i] != c)
-			if (first_word == 1 || s[i - 1] == c)
-			{
-				first_word = 0;
-				size = 0;
-				while (s[i + size] != c && s[i + size])
-					size++;
-				tmp_arr[nb_words] = m_strndup(&s[i], size);
-				nb_words++;
-			}
-		i++;
+		while (str[j] && str[j] != c)
+			j++
+		arr[word] = ft_strndup(&(str[i]), j - i);
+		while (str[j] && str[j] == c)
+			j++;
+		i = j;
+		word++;
 	}
+	arr[word] = "\0";
 }
 
 char		**ft_strsplit(char const *s, char c)
 {
-	char	**tmp_arr;
-	int		nb_words;
+	char	**arr;
+	int	nb_words;
+	int	i;
 
 	if (!s)
 		return (NULL);
-	nb_words = count_words(s, c);
-	tmp_arr = malloc(sizeof(char*) * (nb_words + 1));
-	if (tmp_arr == NULL)
+	i = 0;
+	while (s[i] == c)
+		i++;
+	nb_words = count_words(&(s[i]), c);
+	arr = malloc(sizeof(char *) * (nb_words + 1));
+	if (arr == NULL)
 		m_exit_error(NULL, errno);
-	add_words(s, c, tmp_arr);
-	tmp_arr[nb_words] = 0;
-	return (tmp_arr);
+	fill_arr(s, c, arr);
+	return (arr);
 }
