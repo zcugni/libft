9b75270f76272ac4d4ef6f31b28ed067620c33e2
Zoé Cugni <zcugni@student.42.fr>
2019-01-14 14:55:26 +0100

    Update m_strsplit_white
    L'ancienne version était moins concise

--- srcs/str/m_strsplit_white.c
+++ srcs/str/m_strsplit_white.c
@@ -7,64 +7,59 @@
 
 static int	count_words(char const *s)
 {
-	int first_word;
 	int nb_words;
 	int i;
 
 	i = 0;
-	first_word = 1;
 	nb_words = 0;
 	while (s[i])
 	{
-		if (!ft_iswhitespace(s[i]))
-			if (first_word == 1 || ft_iswhitespace(s[i - 1]))
-			{
-				first_word = 0;
-				nb_words++;
-			}
-		i++;
+		while (s[i] && !ft_iswhitespace(s[i]))
+			i++;
+		nb_words ++;
+		while (s[i] && ft_iswhitespace(s[i]))
+			i++;
 	}
 	return (nb_words);
 }
 
-static void	add_words(char const *s, char **tmp_arr)
+static void	fill_arr(char const *s, char **arr)
 {
-	int		first_word;
-	size_t	size;
-	int		nb_words;
+	size_t	j;
+	size_t	word;
 	size_t	i;
 
-	nb_words = 0;
-	first_word = 1;
+	word = 0;
+	j = 0;
 	i = 0;
 	while (s[i])
 	{
-		if (!ft_iswhitespace(s[i]))
-			if (first_word == 1 || ft_iswhitespace(s[i - 1]))
-			{
-				first_word = 0;
-				size = 0;
-				while (s[i + size] && !ft_iswhitespace(s[i + size]))
-					size++;
-				tmp_arr[nb_words] = m_strndup(&s[i], size);
-				nb_words++;
-			}
-		i++;
+		while (s[i] && !ft_iswhitespace(s[i]))
+			i++;
+		arr[word] = ft_strndup(&(s[i]), j - i);
+		while (ft_iswhitespace([i]))
+			i++;
+		i = j;
+		word++;
 	}
+	arr[word] = "\0";
 }
 
 char		**m_strsplit_white(char const *s)
 {
-	char	**tmp_arr;
-	int		nb_words;
-
+	char	**arr;
+	int	nb_words;
+	int	i;
+	
 	if (!s)
 		return (NULL);
-	nb_words = count_words(s);
-	tmp_arr = malloc(sizeof(char*) * (nb_words + 1));
-	if (tmp_arr == NULL)
+	i = 0;
+	while (ft_ishitespace(s[i]))
+		i++;
+	nb_words = count_words(&(s[i]));
+	arr = malloc(sizeof(char *) * (nb_words + 1));
+	if (!arr)
 		m_exit_error(NULL, errno);
-	add_words(s, tmp_arr);
-	tmp_arr[nb_words] = 0;
-	return (tmp_arr);
+	fil_arr(s, arr);
+	return (arr);
 }
